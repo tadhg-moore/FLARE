0e29c262c668a7b672cf40f2bc971d7125ba4ef5
Tadhg Moore <tadhg.moore@dkit.ie>
2019-12-23 14:31:21 +0000

    Added R scripts

---
+++ Rscripts/met_downscale/process_downscale_GEFS_fgh.R
@@ -0,0 +1,135 @@
+# --------------------------------------
+# purpose: run downscaling processes
+# Creator: Laura Puckett, December 21 2018
+# contact: plaura1@vt.edu
+# --------------------------------------
+# summary: this calls all other functions required to downscale future GEFS forecasts to a specific site using the relationship between saved forecasts and site observations
+# --------------------------------------
+process_downscale_GEFS <- function(folder,
+                                   noaa_location,
+                                   input_met_file,
+                                   working_directory,
+                                   sim_files_folder,
+                                   n_ds_members,
+                                   n_met_members,
+                                   file_name,
+                                   local_tzone,
+                                   FIT_PARAMETERS,
+                                   DOWNSCALE_MET,
+                                   met_downscale_uncertainty,
+                                   compare_output_to_obs,
+                                   VarInfo,
+                                   replaceObsNames,
+                                   downscaling_coeff,
+                                   full_time_local,
+                                   first_obs_date,
+                                   last_obs_date,
+                                   input_met_file_tz){
+  # -----------------------------------
+  # 0. Source necessary files
+  # -----------------------------------
+  
+  path.met.ds.folder <- paste0(folder,"/","Rscripts/met_downscale/")
+  
+  for(f in list.files(path = path.met.ds.folder, pattern="*.R")){
+    if(f != "process_downscale_GEFS.R" & f != "run_met_downscaling.R"& f != "main_downscaling.R"){
+      source(paste0(path.met.ds.folder, f))
+    }
+  }
+  
+  # -----------------------------------
+  # 1. Load & reformat observational data
+  # -----------------------------------
+  input_met_file = "C:\\Users\\mooret\\Desktop\\flare_feeagh\\feeagh_data\\met/GLM_met_obs.csv"
+  noaa_location <-  "C:\\Users\\mooret\\OneDrive - Dundalk Institute of Technology\\PROGNOS_offline\\CareyVT\\feeagh-noaa"
+  input_met_file_tz = 'GMT'
+  local_tzone = 'GMT'
+  
+  obs.file.path = input_met_file
+  for.file.path = noaa_location
+  
+  # obs.data <- read_csv(obs.file.path, skip = 4, col_names = FALSE)
+  obs.data <- read_csv(obs.file.path)
+  # d_names <- read_csv(obs.file.path, skip = 1, col_names = TRUE, n_max = 5)
+  # names(obs.data) <- names(d_names)
+  
+  VarNames = as.vector(VarInfo$VarNames)
+  
+  maxTempC = 35 # an upper bound of realistic temperature for the study site in deg C
+  minTempC = -10 # an lower bound of realistic temperature for the study site in deg C
+  observations <- obs.data %>%
+  #   # plyr::rename(replaceObsNames) %>%
+    dplyr::mutate(TIMESTAMP = as.character(TIMESTAMP)) %>%
+    dplyr::mutate_at(VarNames, as.numeric) %>%
+    dplyr::mutate(timestamp = as_datetime(TIMESTAMP,
+                                          tz = input_met_file_tz)) %>%
+    dplyr::mutate(AirTemp = AirTemp + 273.15,# convert from C to Kelvin
+                  Rain = Rain* 60 * 24/1000) %>% # convert from mm to m
+    dplyr::select(timestamp, VarNames)
+  observations$timestamp <- with_tz(observations$timestamp, local_tzone)
+  # observations <- observations %>%
+  #   dplyr::mutate(ShortWave = ifelse(ShortWave < 0, 0, ShortWave),
+  #                 RelHum = ifelse(RelHum <0, 0, RelHum),
+  #                 RelHum = ifelse(RelHum > 100, 100, RelHum),
+  #                 AirTemp = ifelse(AirTemp> 273.15 + maxTempC, NA, AirTemp),
+  #                 AirTemp = ifelse(AirTemp < 273.15 + minTempC, NA, AirTemp),
+  #                 LongWave = ifelse(LongWave < 0, NA, LongWave),
+  #                 WindSpeed = ifelse(WindSpeed <0, 0, WindSpeed)) %>%
+  #   filter(is.na(timestamp) == FALSE)
+  # 
+  # observations$RelHum <- na_interpolation(observations$RelHum)
+  # observations$AirTemp <- na_interpolation(observations$AirTemp)
+  # observations$LongWave <- na_interpolation(observations$LongWave)
+  # observations$WindSpeed <- na_interpolation(observations$WindSpeed)
+  
+  rm(obs.data)
+  hrly.obs <- observations#observations %>% aggregate_obs_to_hrly()
+  
+  # -----------------------------------
+  # 1. Fit Parameters
+  # -----------------------------------
+  
+  if(FIT_PARAMETERS){
+    print("Fit Parameters")
+    fit_downscaling_parameters(observations,
+                               for.file.path = noaa_location,
+                               working_directory,
+                               VarNames,
+                               VarNamesStates,
+                               replaceObsNames,
+                               PLOT = TRUE,
+                               local_tzone,
+                               VarInfo,
+                               first_obs_date,
+                               last_obs_date)
+  }
+  # -----------------------------------
+  # 2. Process GEFS
+  # -----------------------------------
+  met_forecast_output <- process_GEFS(file_name,
+                                      n_ds_members,
+                                      n_met_members,
+                                      sim_files_folder,
+                                      in_directory = noaa_location,
+                                      out_directory = working_directory,
+                                      local_tzone,
+                                      VarInfo,
+                                      replaceObsNames,
+                                      hrly.observations = hrly.obs,
+                                      DOWNSCALE_MET,
+                                      FIT_PARAMETERS,
+                                      met_downscale_uncertainty,
+                                      WRITE_FILES = TRUE,
+                                      downscaling_coeff,
+                                      full_time_local)
+  files = met_forecast_output[[1]]
+  output = met_forecast_output[[2]]
+  if(compare_output_to_obs == TRUE){
+    "comparing forecast output to obs"
+    compare_output_to_obs(output, hrly.obs)
+  }
+  return(files)
+}
+
+
+
---
+++ forecast_creation_script.R
@@ -0,0 +1,287 @@
+#missing noaa files 
+#20181221gep_all_00z.csv to 20181229gep_all_00z.csv
+
+do_step_1 <- TRUE
+do_step_2 <- TRUE
+do_step_3 <- FALSE
+###Step 0 ####################
+#Load required elements
+
+if (!"mvtnorm" %in% installed.packages()) install.packages("mvtnorm")
+if (!"ncdf4" %in% installed.packages()) install.packages("ncdf4")
+if (!"lubridate" %in% installed.packages()) install.packages("lubridate")
+if (!"RCurl" %in% installed.packages()) install.packages("RCurl")
+if (!"testit" %in% installed.packages()) install.packages("testit")
+if (!"imputeTS" %in% installed.packages()) install.packages("imputeTS")
+if (!"tidyverse" %in% installed.packages()) install.packages("tidyverse")
+
+library(mvtnorm)
+library(ncdf4)
+library(lubridate)
+library(RCurl)
+library(testit)
+library(imputeTS)
+library(tidyverse)
+library(tools)
+
+data_location <<- "/Users/quinn/Dropbox/Research/SSC_forecasting/SCC_data/"
+code_folder <<- "/Users/quinn/Dropbox/Research/SSC_forecasting/FLARE/"
+forecast_location <<- "/Users/quinn/Dropbox/Research/SSC_forecasting/flare_first_paper_revision/model_output/"
+execute_location <<- "/Volumes/ramdisk/revisions/"
+
+source(paste0(forecast_location,"/","configure_FLARE.R"))
+source(paste0(code_folder, "/", "Rscripts/run_flare.R"))
+source(paste0(code_folder, "/", "Rscripts/plot_forecast.R"))
+
+spin_up_days <- 0
+
+start_day <- as_date("2018-07-12")
+forecast_start<- as_date("2018-08-27")
+start_time_local <-"07:00:00"
+holder1 <- start_day
+holder2 <- forecast_start
+while(forecast_start < as_date("2019-11-01")){
+  start_day <- forecast_start
+  forecast_start <- forecast_start + days(1)
+  if(forecast_start < as_date("2019-11-01")){
+    holder1 <- c(holder1, start_day)
+    holder2 <- c(holder2, forecast_start)
+  }
+}
+
+forecast_days_vector <- rep(16, length(holder1))
+#missing_forecasts <- seq(as_date("2018-12-21"), as_date("2018-12-29"), 1)
+
+forecast_days_vector[1] <- 0
+#forecast_days_vector[holder2 %in% missing_forecasts] <- 0
+
+forecasting_timings <- data.frame(holder1,holder2,forecast_days_vector)
+
+start_time_local <- "07:00:00"
+partition_dates <- as.POSIXct(c("2018-09-24","2018-10-01","2018-11-01", "2018-12-01"), format = "%Y-%m-%d")
+partition_dates <- as_date(c("2018-08-27","2018-09-03"))
+partition_restart_files <- rep(NA, length(partition_dates))
+
+sim_name_list <- c('FLARE1_ALL_UNCERT',
+                   'FLARE1_INIT_UNCERT_WDATA',
+                   'FLARE1_INIT_UNCERT_WODATA',
+                   'FLARE1_PROCESS_UNCERT',
+                   'FLARE1_NOAA_DRIVER_UNCERT',
+                   'FLARE1_DS_DRIVER_UNCERT',
+                   'FLARE1_PARAM_UNCERT',
+                   'FLARE1_INFLOW_UNCERT')
+
+uncert_mode_list <- c(1,
+                      5,
+                      6,
+                      3,
+                      4,
+                      8,
+                      7,
+                      10)
+
+###Step 1 ####################
+
+#RUN FROM JANUARY 1, 2018 to AUGUS
+if(do_step_1){
+  
+  sim_name <- 'FLARE1_SPIN_UP'
+  start_day_local <-  as_date(forecasting_timings[1,1])
+  
+  forecast_start_day_local <- as_date(forecasting_timings[1,2])
+  
+  hist_days <- as.numeric(difftime(as_date(forecast_start_day_local),as_date(start_day_local)))
+  
+  forecast_days <- forecasting_timings[1,3]
+  restart_file <- NA
+  forecast_sss_on <- TRUE
+  
+  out <- run_flare(start_day_local,
+                   start_time_local,
+                   forecast_start_day_local,
+                   sim_name = sim_name,
+                   hist_days = hist_days,
+                   forecast_days = forecast_days,
+                   spin_up_days = spin_up_days,
+                   restart_file = restart_file,
+                   code_folder = code_folder,
+                   forecast_location = forecast_location,
+                   execute_location = execute_location,
+                   push_to_git = push_to_git,
+                   pull_from_git = pull_from_git,
+                   data_location = data_location,
+                   n_enkf_members = n_enkf_members,
+                   n_ds_members = n_ds_members,
+                   include_wq = include_wq,
+                   use_ctd = use_ctd,
+                   uncert_mode = uncert_mode,
+                   cov_matrix = cov_matrix,
+                   downscaling_coeff = downscaling_coeff,
+                   GLMversion = GLMversion,
+                   DOWNSCALE_MET = DOWNSCALE_MET,
+                   FLAREversion = FLAREversion,
+                   met_ds_obs_start = met_ds_obs_start,
+                   met_ds_obs_end = met_ds_obs_end,
+                   modeled_depths = modeled_depths,
+                   forecast_sss_on = forecast_sss_on)
+  
+  
+  plot_forecast(pdf_file_name = unlist(out)[2],
+                output_file = unlist(out)[1],
+                include_wq = include_wq,
+                forecast_days = forecast_days,
+                code_folder = code_folder,
+                save_location = forecast_location,
+                data_location = data_location,
+                plot_summaries = TRUE,
+                push_to_git = push_to_git,
+                pull_from_git = pull_from_git,
+                use_ctd = use_ctd,
+                modeled_depths = modeled_depths)
+  
+  step1_out <- unlist(out)[1]
+  save(step1_out, file = paste0(forecast_location,"/step1_out.Rdata"))
+}
+
+### Step 2 ####
+
+if(do_step_2){
+  
+  load(file = paste0(forecast_location,"/step1_out.Rdata"))
+  restart_file <- step1_out
+  sim_name <- 'FLARE1_FOR'
+  
+  for(i in 2:nrow(forecasting_timings)){  
+    
+    source(paste0(forecast_location,"/","configure_FLARE.R"))
+    
+    hist_days <- as.numeric(difftime(as_date(forecasting_timings[i,2]),as_date(forecasting_timings[i,1])))
+    start_day_local <- forecasting_timings[i,1]
+    forecast_start_day_local <- forecasting_timings[i,2]
+    forecast_days <- forecasting_timings[i,3]
+    print(forecasting_timings)
+    forecast_sss_on <- FALSE
+    use_ctd <- FALSE
+    
+    out <- run_flare(start_day_local,
+                     start_time_local,
+                     forecast_start_day_local,
+                     sim_name = sim_name,
+                     hist_days = hist_days,
+                     forecast_days = forecast_days,
+                     spin_up_days = spin_up_days,
+                     restart_file = restart_file,
+                     code_folder = code_folder,
+                     forecast_location = forecast_location,
+                     execute_location = execute_location,
+                     push_to_git = push_to_git,
+                     pull_from_git = pull_from_git,
+                     data_location = data_location,
+                     n_enkf_members = n_enkf_members,
+                     n_ds_members = n_ds_members,
+                     include_wq = include_wq,
+                     use_ctd = use_ctd,
+                     uncert_mode = uncert_mode,
+                     cov_matrix = cov_matrix,
+                     downscaling_coeff = downscaling_coeff,
+                     GLMversion = GLMversion,
+                     DOWNSCALE_MET = DOWNSCALE_MET,
+                     FLAREversion = FLAREversion,
+                     met_ds_obs_start = met_ds_obs_start,
+                     met_ds_obs_end = met_ds_obs_end,
+                     modeled_depths = modeled_depths,
+                     forecast_sss_on = forecast_sss_on)
+    
+    restart_file <- unlist(out)[1]
+    
+    if(start_day_local %in% partition_dates){
+      index <- which(partition_dates == start_day_local)
+      partition_restart_files[index] <- restart_file
+    }
+    
+    plot_forecast(pdf_file_name = unlist(out)[2],
+                  output_file = unlist(out)[1],
+                  include_wq = include_wq,
+                  forecast_days = forecast_days,
+                  code_folder = code_folder,
+                  save_location = forecast_location,
+                  data_location = data_location,
+                  plot_summaries = TRUE,
+                  push_to_git = push_to_git,
+                  pull_from_git = pull_from_git,
+                  use_ctd = use_ctd,
+                  modeled_depths = modeled_depths)
+  }
+  
+  save(partition_restart_files, partition_dates, file = paste0(forecast_location,"/partition_restart_files.Rdata"))
+}
+
+#Step 3 
+
+if(do_step_3){
+  load(paste0(forecast_location,"/partition_restart_files.Rdata"))
+  source(paste0(forecast_location,"/","configure_FLARE.R"))
+  
+  #for(i in 1:length(partition_dates)){
+    for(i in 1:1){
+    
+    hist_days <- 6
+    start_day_local <- partition_dates[i]
+    forecast_start_day_local <- partition_dates[i] + days(hist_days)
+    forecast_days <- 16
+    forecast_sss_on <- FALSE
+    use_ctd <- FALSE
+    
+    restart_file <- partition_restart_files[i]
+
+    for(ii in 2:length(sim_name_list)){
+      
+      source(paste0(forecast_location,"/","configure_FLARE.R"))
+      
+      sim_name <- sim_name_list[ii]
+      
+      out1 <- run_flare(start_day_local,
+                        start_time_local,
+                        forecast_start_day_local,
+                        sim_name = sim_name,
+                        hist_days = hist_days,
+                        forecast_days = forecast_days,
+                        spin_up_days = spin_up_days,
+                        restart_file = restart_file,
+                        code_folder = code_folder,
+                        forecast_location = forecast_location,
+                        execute_location = execute_location,
+                        push_to_git = push_to_git,
+                        pull_from_git = pull_from_git,
+                        data_location = data_location,
+                        n_enkf_members = n_enkf_members,
+                        n_ds_members = n_ds_members,
+                        include_wq = include_wq,
+                        use_ctd = use_ctd,
+                        uncert_mode = uncert_mode_list[ii],
+                        cov_matrix = cov_matrix,
+                        downscaling_coeff = downscaling_coeff,
+                        GLMversion = GLMversion,
+                        DOWNSCALE_MET = DOWNSCALE_MET,
+                        FLAREversion = FLAREversion,
+                        met_ds_obs_start = met_ds_obs_start,
+                        met_ds_obs_end = met_ds_obs_end,
+                        modeled_depths = modeled_depths,
+                        forecast_sss_on = forecast_sss_on)
+      
+      plot_forecast(pdf_file_name = unlist(out1)[2],
+                    output_file = unlist(out1)[1],
+                    include_wq = include_wq,
+                    forecast_days = forecast_days,
+                    code_folder = code_folder,
+                    save_location = forecast_location,
+                    data_location = data_location,
+                    plot_summaries = TRUE,
+                    push_to_git = push_to_git,
+                    pull_from_git = pull_from_git,
+                    use_ctd = use_ctd,
+                    modeled_depths = modeled_depths)
+      
+    }
+  }
+}
